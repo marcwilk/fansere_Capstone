ec16d8841dcb3e3e31578239e14e053695344dea
Sean Tansey <sean.d.tansey@gmail.com>
2019-03-25 11:38:39 -0600

    fixed roster and users bug

--- components/feed/mainFeed.js
+++ components/feed/mainFeed.js
@@ -11,7 +11,7 @@ export default class Feed extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      userId: 'ldPba8sgFXeM2q3SF2u6CupAy7v2',
+      userId: 'soalBDZkkoMBzJAd5EdQsE5x8113',
       userName:'Fansere Guy',
       nearbyUsers: [],
       isModalVisible: false,
@@ -31,6 +31,7 @@ export default class Feed extends React.Component {
                result = arr
            }
        }
+       //console.log(result)
        return result
    }
 
@@ -39,12 +40,17 @@ export default class Feed extends React.Component {
       firebase.firestore().collection('conversations')
         .onSnapshot(snapshot => {
           let newDocs = snapshot.docChanges()
+          let conversations = []
           newDocs.forEach(doc => {
             let releventInfo = this.checkForUserId(doc.doc.data().members)
+            //console.log(releventInfo)
             if (releventInfo) {
               let otherUser = doc.doc.data().members.filter(conv => conv.userId !== this.state.userId)
-              this.setState({conversations: [...this.state.conversations, otherUser[0].userId]})
+              conversations.push(otherUser[0].userId)
+              //this.setState({conversations: [...this.state.conversations, otherUser[0].userId]})
             }
+            this.setState({conversations: conversations})
+            console.log(this.state.conversations)
           })
         })
         firebase.firestore().collection('users')
